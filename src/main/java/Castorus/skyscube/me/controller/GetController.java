1458c8bf229f3aa4f3c10722303c0f6e3ab9a9ff
SkyScube <leolothore969@gmail.com>
2025-05-22 11:56:09 +0200

    Gestion de ticket

--- src/main/java/Castorus/skyscube/me/controller/GetController.java
+++ src/main/java/Castorus/skyscube/me/controller/GetController.java
@@ -1,12 +1,19 @@
 package Castorus.skyscube.me.controller;
 
+import Castorus.skyscube.me.DBManager.TicketsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class GetController {
+    @Autowired
+    private TicketsService ticketsService;
+
     @GetMapping("/")
-    public String home() {
+    public String home(ModelMap modelMap) {
+        modelMap.addAttribute("table", ticketsService.getAllTicketss());
         return "index";
     }
 
